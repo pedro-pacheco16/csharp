389e0b0f75f13a6aaf3859bd098f930a0edfa06a
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-23 13:09:58 -0300

    exercicio 1

---
+++ aula02/calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2, divisao;
+
+            Console.WriteLine("digite o primeiro numero:");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo numero:");
+            numero2 = Convert.ToSingle(Console.ReadLine());//conversor de string para float
+            divisao = numero1 / numero2;
+            Console.WriteLine($"{numero1} + {numero2} = " + (numero1 + numero2));//numeros dentro das chaves guarda o valor digitado
+            Console.WriteLine($"{numero1} - {numero2} = " + (numero1 - numero2));
+            Console.WriteLine($"{numero1} * {numero2} = " + (numero1 * numero2));
+            Console.WriteLine($"{numero1} / {numero2} =  {divisao:F2}");
+            Console.WriteLine($"{numero1} ^ {numero2} = " + Math.Pow(numero1, numero2));
+            Console.WriteLine($"raiz quadrada de {numero1} = " + Math.Sqrt(numero2));
+        }
+    }
+}
---
+++ aula02/exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario, abono, novoSalario;
+            Console.WriteLine("digite o salário:");
+            salario = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("digite o abono:");
+            abono = Convert.ToSingle(Console.ReadLine());
+
+            novoSalario = salario + abono;
+
+           Console.WriteLine("novo salário = " + Math.Round(novoSalario,2));
+        }
+    }
+}
