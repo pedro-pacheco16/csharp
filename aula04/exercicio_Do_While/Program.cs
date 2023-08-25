8b6d7e1a14a1a51c06419df1398127d45dc3b980
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-25 15:05:52 -0300

    exercicio 5 Do While

---
+++ aula04/exercicio_Do_While/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercicio_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, somaPositivo = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um número:");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero > 0)
+            {
+                somaPositivo += numero;
+
+            }
+            } while (numero != 0);
+
+            Console.WriteLine($"soma dos número positivos: {somaPositivo}");
+
+        }
+    }
+}
