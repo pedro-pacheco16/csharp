3d57ed8d425f1a2b7dec180cb627a0e9ab1c6362
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-25 22:56:33 -0300

    exercicio animais

--- aula03/animaisex04/Program.cs
+++ aula03/animaisex04/Program.cs
@@ -15,7 +15,7 @@
             Console.WriteLine("Digite a terceira palavra:");
             palavra3 = Console.ReadLine();
 
-            if(palavra1 is not null && palavra2 is not null && palavra3 is not null)
+            if (palavra1 is not null && palavra2 is not null && palavra3 is not null)
             {
                 //verificando se é vertebrado ou invertebrado
                 if (palavra1.Equals("vertebrado"))
@@ -43,12 +43,38 @@
                             Console.WriteLine("vaca");
 
                         }
-                    } 
-                }
-                else
-                {
+                    }
+                    if (palavra1.Equals("invertebrado"))
+                    {
+
+                        if (palavra2.Equals("inseto"))
+                        {
+                            if (palavra3.Equals("Hematófogo"))
+                            {
+                                Console.WriteLine("Pulga");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Lagarta");
+                            }
+                        }
+                        else
+                        {
+                            if (palavra3.Equals("anelídeo"))
+                            {
+                                Console.WriteLine("Sanguessuga");
+                            }
+                            else
+                            {
+                                Console.WriteLine("minhoca");
+
+                            }
+                        }
+
+                    }
 
                 }
+
             }
             else
             {
