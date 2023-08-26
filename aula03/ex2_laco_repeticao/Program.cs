fa297e652000ac34dbcbd1694dab4875a7eddb77
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-25 22:30:04 -0300

    exercicio2_if

---
+++ aula03/ex2_laco_repeticao/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex2_laco_repeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0 && num > 0)
+            {
+                Console.Write($"\nO número {num} é par e positivo! ");
+           
+            } else if (num % 2 == 0 && num < 0)
+            {
+                Console.Write($"\nO número {num} é par e negativo! ");
+            }
+            else if (num % 2 != 0 && num > 0)
+            {
+                Console.Write($"\nO número {num} é impar e positivo! ");
+
+            }
+            else
+            {
+                Console.Write($"\nO número {num} é impar e negativo! ");
+            }
+        }
+    }
+}
