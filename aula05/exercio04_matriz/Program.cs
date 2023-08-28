78300f87d52d4f9f518e08668a6b8349fb0d59a5
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-28 17:17:17 -0300

    Exercicio 04 de matriz

---
+++ aula05/exercio04_matriz/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace exercio04_matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] matriz= new float[10, 4];
+            float[] soma = new float [10];
+
+
+            for (int iLinha = 0; iLinha < matriz.GetLength(0); iLinha++)
+            {
+
+                for (int iColuna = 0; iColuna < matriz.GetLength(1); iColuna++)
+                {
+                    Console.WriteLine($"matriz[{iLinha},{iColuna}] = ");
+                    matriz[iLinha, iColuna] = Convert.ToSingle(Console.ReadLine());
+
+                }
+            }
+            for (int iLinha = 0; iLinha < matriz.GetLength(0); iLinha++)
+            {
+
+                for (int iColuna = 0; iColuna < matriz.GetLength(1); iColuna++)
+                {
+                    soma[iLinha] += matriz[iLinha,iColuna];
+                }
+
+                soma[iLinha] = soma[iLinha] / 4;
+            }
+            for(int iLinha = 0; iLinha < matriz.GetLength(0); iLinha++)
+            {
+                Console.WriteLine($"As médias são: {soma[iLinha]:F1}");
+            }
+        }
+    }
+}
