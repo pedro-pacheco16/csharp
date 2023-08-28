0ad9d30f4e9d084ec488eb40cbee905d7b2631d7
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-28 18:30:35 -0300

    exercicio 06 Do While

---
+++ aula04/exercicio06_Do_While/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+
+namespace exercicio06_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           float media = 0,numero,soma = 0,total = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um número:");
+                numero = Convert.ToSingle(Console.ReadLine());
+
+                if (numero % 3 == 0 && numero != 0)
+                {
+                    soma += numero;
+                    total++;
+                }
+
+            } while (numero != 0);
+
+            media = soma / total;
+
+            Console.WriteLine($"Média dos multiplos de 3: {media:F2}");
+
+        }
+    }
+}
