4ea181365e4dc26d718674779e2107d38f3b6398
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-28 14:28:22 -0300

    Exercicio 1 vetor

---
+++ aula05/exer01_vetores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace exer01_vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int num = 0;
+
+     
+            Console.WriteLine($"Digite um número:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int posicao = Array.IndexOf(vetor, num);
+
+            if (posicao >= 0)
+                {  
+                    Console.WriteLine($"\na posição do numero é:" + posicao);
+                }
+                else
+                {
+                    Console.WriteLine($"\nNão foi encontrado!");
+                }
+            }
+        }
+    }
