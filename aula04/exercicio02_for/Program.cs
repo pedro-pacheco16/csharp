ec155a326d65fd6192fe83769e70e4cf3f12b79c
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-28 14:33:11 -0300

    resolvendo problemas do git

---
+++ aula04/exercicio02_for/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace exercicio02_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPares = 0, numImpares = 0, numeros = 10;
+
+            for(int i = 1; i <= numeros; i++)
+            {
+                Console.WriteLine("Digite os numeros:");
+                numeros = Convert.ToInt32(Console.ReadLine());
+
+                if(numeros % 2 == 0)
+                {
+                    numPares++;
+                }
+                else
+                {
+                    numImpares++;
+                }
+
+            }
+
+            Console.WriteLine($"Total de números Pares: {numPares}");
+            Console.WriteLine($"Total de números Pares: {numImpares}");
+        }
+    }
+}
