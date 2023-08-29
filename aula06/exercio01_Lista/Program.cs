00d76404fb422c450a056c3bacffe8329109c023
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-29 12:43:51 -0300

    Exercicio collection List

---
+++ aula06/exercio01_Lista/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace exercio01_Lista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cores = new List<string>();
+
+            Console.WriteLine("Digite 5 cores: ");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                cores.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n");
+
+            foreach(var cor in cores)
+            {
+                Console.WriteLine(cor);
+            }
+
+            Console.WriteLine("\n");
+
+            cores.Sort();
+
+            foreach (var cor in cores)
+            {
+                Console.WriteLine(cor);
+            }
+        }
+        
+    }    
+
+}
