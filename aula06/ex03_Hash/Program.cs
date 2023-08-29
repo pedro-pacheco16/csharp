d55ae7e1939eed2daeb1ba2aa0ad4258b68c0e38
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-29 12:56:49 -0300

    Exercicio 03 HshSet

---
+++ aula06/ex03_Hash/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ex03_Hash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> valores = new HashSet<int>();
+
+            Console.WriteLine("Digite 10 números: ");
+
+            for (int i = 1; i <= 10; i++)
+            {
+               valores.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var val in valores)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
