b46de5e9fb4ac5abd1cc9c4dfbf6c0a790d0dee0
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-08-31 15:45:21 -0300

    atividade 02 classes

---
+++ aula08/Atividade02_POO/Program.cs
@@ -0,0 +1,18 @@
+﻿using AtivRMade02_POO.model_02;
+
+namespace Atividade02_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            funcionario funcionario1 = new funcionario(1049, "Pedro Pacheco", "Mecânico de Usinagem", 10000, "Biscoitos Bela Vista");
+
+            funcionario1.visualizar();
+
+            funcionario funcionario2 = new funcionario(1050, "Carlos", "Eletricista", 15000, "Biscoitos Bela Vista");
+
+            funcionario2.visualizar();
+        }
+    }
+}
---
+++ aula08/Atividade02_POO/model_02/funcionario.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtivRMade02_POO.model_02
+{
+    public class funcionario
+    {
+        private int RM;
+        private string nome = string.Empty;
+        private string cargo = string.Empty;
+        private decimal salario;
+        private string empresa = string.Empty;
+
+        public funcionario(int RM, string nome, string cargo, int salario, string empresa)
+        {
+            this.RM = RM;
+            this.nome = nome;
+            this.cargo = cargo;
+            this.salario = salario;
+            this.empresa = empresa;
+        }
+        public int GetRM()
+        {
+            return RM;
+        }
+        public void SetRM(int RM)
+        {
+            this.RM = RM;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string Getcargo()
+        {
+            return cargo;
+        }
+
+        public void Setcargo(string cargo)
+        {
+            this.cargo = cargo;
+        }
+
+        public decimal Getsalario()
+        {
+            return salario;
+        }
+
+        public void Setsalario(decimal salario)
+        {
+            this.salario = salario;
+        }
+
+        public string Getempresa()
+        {
+            return empresa;
+        }
+
+        public void Setempresa(string empresa)
+        {
+            this.empresa = empresa;
+        }
+
+        public void visualizar()
+        {
+            Console.WriteLine("\n\n*********************************************************************");
+            Console.WriteLine("Dados do Funcionário:");
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine("Numero do RM: " + this.RM);
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("cargo: " + cargo);
+            Console.WriteLine("salario: " + this.salario);
+            Console.WriteLine("empresa: " + this.empresa);
+
+        }
+    }
+}
