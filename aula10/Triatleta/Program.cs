3c9f3c26eab100f6da21009a6546189022560368
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-09-05 23:22:17 -0300

    realizando interface

---
+++ aula10/Triatleta/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Triatleta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triatleta t1 = new Triatleta("Pedro");
+
+            t1.Aquecer();
+            t1.Correr();
+            t1.Nadar();
+            t1.pedalar();
+            t1.Cansou();
+        }
+    }
+}
