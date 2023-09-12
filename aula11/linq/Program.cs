110b2bfb85031a7a253a8187fc7b391ed3d5a329
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-09-12 20:51:45 -0300

    exemplo LINQ

---
+++ aula11/linq/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<string> nomes = new List<string>() {"Matheus S",
+                "Bugson",
+                "Zézinho",
+                "Fulano",
+                "Ciclano",
+                "Pedro A",
+                "Melissa",
+                "Duda",
+                "Junin",
+                "Zebug"
+            };
+
+            var numerosAoCubo = numeros.Select(n => Math.Pow(n, 3));
+
+            Console.WriteLine("Números elevados ao cubo: " + string.Join(", ", numerosAoCubo));
+
+            var numerosImpares = from numero in numeros
+                                 where numero % 2 != 0
+                                 select numero;
+
+            Console.WriteLine("Números Ímpares: " + string.Join(", ", numerosImpares));
+
+            var nomesIniciamMZ = (from nome in nomes
+                                  where nome.StartsWith('M') ||
+                                        nome.StartsWith('Z')
+                                  select nome).ToList();
+
+            nomesIniciamMZ.ForEach(n => Console.WriteLine(n));
+
+            //Console.WriteLine("Nomes que iniciam com M ou Z: " + string.Join(", ", nomesIniciamMZ));
+
+
+            var nomesContemC = from nome in nomes
+                               where nome.ToUpper().Contains('C')
+                               select nome;
+
+            Console.WriteLine("Nomes que contém a letra C: " + string.Join(", ", nomesContemC));
+
+
+            var numerosCrescente = from numero in numeros
+                                   orderby numero
+                                   select numero;
+
+            Console.WriteLine("Números em ordem crescente: " + string.Join(", ", numerosCrescente));
+
+
+            var numerosDecrescente = from numero in numeros
+                                     orderby numero descending
+                                     select numero;
+
+            Console.WriteLine("Números em ordem Decrescente: " + string.Join(", ", numerosDecrescente));
+
+
+            var nomesSemRepeticao = from nome in nomes
+                               .Distinct()
+                                    select nome;
+
+            Console.WriteLine("Nomes sem duplicidade: " + string.Join(", ", nomesSemRepeticao));
+
+
+            var contarPedro = nomes.Count(n => n.Contains("Pedro"));
+
+            Console.WriteLine("Quantidade de Pedro na list: " + contarPedro);
+
+
+            var primeiroPedro = nomes.First(n => n.Contains("Matheus"));
+
+            Console.WriteLine("Primeiro Pedro da list: " + primeiroPedro);
+        }
+    }
+}
